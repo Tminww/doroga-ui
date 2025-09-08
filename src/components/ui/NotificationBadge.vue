54c1b922cdfdd9b310600ffcf1025fc5906facb7
Serafim Oleynik <serafim.oleynik.5777@yandex.ru>
2025-09-08 12:40:22 +0300

    add: Добавил компонент для отображения того, что какие-то данные есть

---
+++ src/components/ui/NotificationBadge.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="icon-container">
+    <slot></slot>
+    <!-- Здесь размещается иконка -->
+    <div v-if="hasNotification" class="notification-badge"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+interface NotificationBadgeProps {
+  hasNotification: boolean
+}
+withDefaults(defineProps<NotificationBadgeProps>(), { hasNotification: false })
+</script>
+
+<style scoped>
+.icon-container {
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+}
+
+.notification-badge {
+  position: absolute;
+  right: 3px;
+  top: 3px;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+
+  background-color: red;
+}
+</style>
